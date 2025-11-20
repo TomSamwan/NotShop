478d663885b9ead4a0f4ea387b7b9e3460ab7f76
TomSamwan <114116342+TomSamwan@users.noreply.github.com>
2025-11-20 18:44:58 +1100

    rename to NotShop

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5173",
         changeOrigin: true,
-        reqrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
